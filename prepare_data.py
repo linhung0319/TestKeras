4ccc6ebb735d7aabf494653eb000bd48adaf2df5
林鴻 <linhung0319@gmail.com>
2018-09-20 11:52:01 +0800

    Create function generate_data
    function generate_data loads iris dataset and splits the dataset into training set and testing set

--- prepare_data.py
+++ prepare_data.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python2
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+def generate_data(datasets_name, test_size=0.33, random_state=0):
+    if (datasets_name.lower() == 'iris'):
+        data = datasets.load_iris()
+    x_tr, x_te, y_tr, y_te = train_test_split(data.data,
+                                              data.target,
+                                              test_size=test_size,
+                                              random_state=random_state)
+    return x_tr, x_te, y_tr, y_te
+
+if __name__ == '__main__':
+    main()
